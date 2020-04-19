91058cb073ab31cf60f108aa0980215dd336393c
martial <martial.orli@gmx.fr>
2020-04-19 12:03:07 +0200

    add id

--- src/main/java/war/MyResource.java
+++ src/main/java/war/MyResource.java
@@ -82,6 +82,23 @@ public class MyResource {
   	 	stmt.executeUpdate("CREATE TABLE IF NOT EXISTS compte (name VARCHAR(100), amount float, lastRisk int)");
           return "Ok";
       }
+  
+  @Path("addPrimary")    
+  @GET
+  @Produces(MediaType.TEXT_PLAIN)
+      public String addPrimary() {
+	  try {
+  		Connection connection = getConnection();
+
+  		Statement stmt = connection.createStatement();
+  		stmt.executeUpdate("DELETE FROM compte WHERE lastRisk = 0;");
+  	 	stmt.executeUpdate("ALTER TABLE compte ADD COLUMN id SERIAL PRIMARY KEY;");
+        return "Ok";
+	  }catch(Exception e) {
+		  System.err.println(e);
+		  return "error";
+	  }
+      }
 
 
 
