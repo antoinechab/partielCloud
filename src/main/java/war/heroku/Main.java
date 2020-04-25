e2f73e4b697be049fc79b57435f231e7054858b3
martial <martial.orli@gmx.fr>
2020-04-25 11:08:29 +0200

    jsp et jetty

--- src/main/java/war/heroku/Main.java
+++ src/main/java/war/heroku/Main.java
@@ -9,7 +9,7 @@ import org.eclipse.jetty.webapp.WebAppContext;
  */
 public class Main {
 
-    public static void main(String[] args) {
+   /* public static void main(String[] args) {
     	try {
         // The port that we should run on can be set into an environment variable
         // Look for that variable and default to 8080 if it isn't there.
@@ -40,5 +40,30 @@ public class Main {
     }catch(Exception e){
     	System.err.println("Erreur au démarrage: " + e);
     }
-    }
+    }*/
+	
+	 public static void main(String[] args) throws Exception {
+		 
+	        // 1. Creating the server on port 8080
+	        Server server = new Server(8080);
+	 
+	        // 2. Creating the WebAppContext for the created content
+	        WebAppContext ctx = new WebAppContext();
+	        ctx.setResourceBase("src/main/webapp");
+	        ctx.setContextPath("/");
+	         
+	        //3. Including the JSTL jars for the webapp.
+	        ctx.setAttribute("org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern",".*/[^/]*jstl.*\\.jar$");
+	     
+	        //4. Enabling the Annotation based configuration
+	        org.eclipse.jetty.webapp.Configuration.ClassList classlist = org.eclipse.jetty.webapp.Configuration.ClassList.setServerDefault(server);
+	        classlist.addAfter("org.eclipse.jetty.webapp.FragmentConfiguration", "org.eclipse.jetty.plus.webapp.EnvConfiguration", "org.eclipse.jetty.plus.webapp.PlusConfiguration");
+	        classlist.addBefore("org.eclipse.jetty.webapp.JettyWebXmlConfiguration", "org.eclipse.jetty.annotations.AnnotationConfiguration");
+	         
+	        //5. Setting the handler and starting the Server
+	        server.setHandler(ctx);
+	        server.start();
+	        server.join();
+	 
+	    }
 }
