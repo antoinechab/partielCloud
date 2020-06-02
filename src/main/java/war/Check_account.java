abd86c504ea031d56e2b6bf8fcee3420f74a7e9f
martial <martial.orli@gmx.fr>
2020-06-02 10:18:06 +0200

    Check_account

--- src/main/java/war/Check_account.java
+++ src/main/java/war/Check_account.java
@@ -1,10 +1,20 @@
 package war;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import model.Compte;
 
 @Path("check")
 public class Check_account {
@@ -17,7 +27,52 @@ public class Check_account {
      */
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String getIt() {
+    public String hello() {
         return "Vous voulez connaitre le risque d'un compte? C'est par ici!";
+    }
+    
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response check(@PathParam("id") int idAccount) {
+    	Response response = null;
+    	Connection connection;
+    	try {
+    		//float f = Float.parseFloat(amount);
+    		connection = MyResource.getConnection();
+    	    PreparedStatement pstmt = connection.prepareStatement("SELECT * FROM compte WHERE id = ?");
+    	   // Statement stmt = connection.createStatement();
+    	    pstmt.setInt(1, idAccount);
+    	    ResultSet resultSet = pstmt.executeQuery();
+    	    //response = Response.status(Status.OK).entity("Le compte "+ name + " avec une somme de "+amount+" a été créé avec succes!").build();
+    	    
+    	    while (resultSet.next()) {
+    	    	
+    	    	response = Response.status(Status.OK).entity("Le compte "+ resultSet).build();
+                /*long id = resultSet.get("ID");
+                String name = resultSet.getString("NAME");
+                BigDecimal salary = resultSet.getBigDecimal("SALARY");
+                Timestamp createdDate = resultSet.getTimestamp("CREATED_DATE");
+
+                Employee obj = new Employee();
+                obj.setId(id);
+                obj.setName(name);
+                obj.setSalary(salary);
+                // Timestamp -> LocalDateTime
+                obj.setCreatedDate(createdDate.toLocalDateTime());
+
+                System.out.println(obj);*/
+            }
+    	    pstmt.close();
+    	    connection.close();
+    	    return response;
+    	}catch(NumberFormatException e) {
+    		response = Response.status(Status.UNSUPPORTED_MEDIA_TYPE).entity("Le montant doit-etre un chiffre!").build();
+    		return response;
+    	}catch(Exception e) {
+    		response = Response.status(Status.BAD_REQUEST).entity("Erreur lors de la creation du compte: " +  e).build();
+    		System.err.println(e);
+    		return response;
+    		
+    	}
     }
 }
