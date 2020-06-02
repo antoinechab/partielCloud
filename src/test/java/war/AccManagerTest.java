d308f7f441372e0559ec0c2152076f0e2423f2cd
martial <martial.orli@gmx.fr>
2020-06-02 12:05:51 +0200

    test unitaire

--- src/test/java/war/AccManagerTest.java
+++ src/test/java/war/AccManagerTest.java
@@ -2,7 +2,9 @@ package war;
 
 import static org.junit.Assert.assertEquals;
 
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.Application;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
@@ -20,9 +22,36 @@ public class AccManagerTest extends JerseyTest {
 
     @Test
     public void testGet() {
-    	Response response = target("/AccManager").request()
-    	        .get();
+    	Response response = target("AccManager").request().get();
     	    assertEquals("Http Response should be 200: ", Status.OK.getStatusCode(), response.getStatus());
     }
 
+    @Test
+    public void testPost() {
+    	Response response = target("AccManager/jeanMichel/255").request().post(Entity.text(MediaType.TEXT_PLAIN));
+    	assertEquals("Http Response should be 200: ", Status.OK.getStatusCode(), response.getStatus());
+    	
+    	Response response2 = target("AccManager/jeanMichel/zqz").request().post(Entity.text(MediaType.TEXT_PLAIN));
+    	assertEquals("Http Response should be 415: ", Status.UNSUPPORTED_MEDIA_TYPE.getStatusCode(), response2.getStatus());
+    }
+    
+    @Test
+    public void testDelete() {
+    	Response response = target("AccManager/3").request().delete();
+    	assertEquals("Http Response should be 200: ", Status.OK.getStatusCode(), response.getStatus());
+    	
+    	Response response2 = target("AccManager/zqz").request().delete();
+    	assertEquals("Http Response should be 415: ", Status.UNSUPPORTED_MEDIA_TYPE.getStatusCode(), response2.getStatus());
+    }
+    
+    @Test
+    public void testUdpate() {
+    	Response response = target("AccManager/5/jeanMichel/255/0").request().post(Entity.text(MediaType.TEXT_PLAIN));
+    	assertEquals("Http Response should be 200: ", Status.OK.getStatusCode(), response.getStatus());
+    	
+    	Response response2 = target("AccManager/5/jeanMichel/255/dzd").request().post(Entity.text(MediaType.TEXT_PLAIN));
+    	assertEquals("Http Response should be 415: ", Status.UNSUPPORTED_MEDIA_TYPE.getStatusCode(), response2.getStatus());
+    }
+    
+    
 }
--- src/test/java/war/CheckAccountTest.java
+++ src/test/java/war/CheckAccountTest.java
@@ -1,5 +1,29 @@
 package war;
 
-public class CheckAccountTest {
+import static org.junit.Assert.assertEquals;
+
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.junit.Test;
+
+public class CheckAccountTest extends JerseyTest {
+	
+	  @Override
+	    protected Application configure() {
+	        return new ResourceConfig(CheckAccount.class);
+	    }
+	  
+	  @Test
+	    public void testGet() {
+	    	Response response = target("check/5").request().get();
+	    	assertEquals("Http Response should be 200: ", Status.OK.getStatusCode(), response.getStatus());
+	    	    
+	    	Response response2 = target("check/ffe").request().get();
+	        assertEquals("Http Response should be 415: ", Status.UNSUPPORTED_MEDIA_TYPE.getStatusCode(), response2.getStatus());
+	    }
 
 }
